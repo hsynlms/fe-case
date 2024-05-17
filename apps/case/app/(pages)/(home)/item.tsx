5742a41782103aa0baab840f01c43a029f23893b
Huseyin ELMAS <hsynlms47@gmail.com>
2024-05-18 01:49:08 +0300

    fix: scrolling focused item not working due to missing item `ref` attr

--- apps/case/app/(pages)/(home)/item.tsx
+++ apps/case/app/(pages)/(home)/item.tsx
@@ -1,4 +1,5 @@
 import type { JSX } from 'react'
+import { forwardRef } from 'react'
 import type { AralItemProps } from '@fe-case/aral-select'
 import { highlightSearchTerm } from '@fe-case/aral-select'
 
@@ -6,46 +7,49 @@ import { cls } from '@utils'
 
 import styles from './item.module.scss'
 
-export function Item({
-  text, item, isDisabled, isSelected, isFocused,
-  highlight, select, deselect, onHover
-}: AralItemProps): JSX.Element {
-  return (
-    <div
-      className={cls([
-        styles['item'],
-        isDisabled && styles['item-disabled'],
-        isSelected && styles['item-selected'],
-        isFocused && styles['item-focused']
-      ])}
-      onMouseMove={onHover}
-      onMouseOver={onHover}
-      onClick={() => isSelected ? deselect() : select()}
-    >
-      <div className={styles['item-inner']}>
-        <input type={'checkbox'} checked={isSelected} readOnly />
+export const Item = forwardRef<HTMLInputElement, AralItemProps>(
+  function Item({
+    text, item, isDisabled, isSelected, isFocused,
+    highlight, select, deselect, onHover
+  }, ref): JSX.Element {
+    return (
+      <div
+        ref={ref}
+        className={cls([
+          styles['item'],
+          isDisabled && styles['item-disabled'],
+          isSelected && styles['item-selected'],
+          isFocused && styles['item-focused']
+        ])}
+        onMouseMove={onHover}
+        onMouseOver={onHover}
+        onClick={() => isSelected ? deselect() : select()}
+      >
+        <div className={styles['item-inner']}>
+          <input type={'checkbox'} checked={isSelected} readOnly />
 
-        <img
-          src={item['image']}
-          width={40} height={40}
-          alt={text}
-          className={styles['item-image']}
-        />
-
-        <div>
-          <div
-            dangerouslySetInnerHTML={{
-              __html: highlight
-                ? highlightSearchTerm(text, highlight)
-                : text
-            }}
+          <img
+            src={item['image']}
+            width={40} height={40}
+            alt={text}
+            className={styles['item-image']}
           />
 
-          <div className={styles['item-episodes']}>
-            {item['episode']?.length ?? '-'} episodes
+          <div>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: highlight
+                  ? highlightSearchTerm(text, highlight)
+                  : text
+              }}
+            />
+
+            <div className={styles['item-episodes']}>
+              {item['episode']?.length ?? '-'} episodes
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
+)
